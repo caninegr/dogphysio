1d584c70a6d45ff5a17f6c4d3f73605ad3500acf
Vasileios Pappas <info@canine.gr>
2024-04-29 20:08:43 +0300

    first commit

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import emailjs from "@emailjs/browser";
